c72e971c82cb35088833ed41043ec3fc5c760d14
Akshay R <131320419+Akshay-Coded@users.noreply.github.com>
2023-08-18 23:50:55 +0530

    Hello-world.java

---
+++ Hello-world.java
@@ -0,0 +1,6 @@
+//how can we not start of the first program without printing "Hello World" - It is a sin
+public class main{
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+}
