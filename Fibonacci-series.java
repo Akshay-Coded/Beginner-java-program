1a2fbe2b2fc859434b524c552a9a72b501529db8
Akshay R <131320419+Akshay-Coded@users.noreply.github.com>
2023-08-21 02:24:33 +0530

    Create Fibonacci-series.java

---
+++ Fibonacci-series.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int n,num1 = 0,num2 = 1,num=0;
+        Scanner sc = new Scanner(System.in);  //defining scanner object
+        System.out.print("Enter number of elements in series = ");
+        n=sc.nextInt();
+        if(n==1){
+            System.out.println("The Fibonacci series is: "+num1);
+        }
+        else if(n==2){
+            System.out.println("The Fibonacci series is: "+num1+" "+num2);
+        }
+        else{
+            System.out.print("The Fibonacci series is: "+num1+" "+num2+" ");
+            for(int i = 0;i < n-2;i++){
+                num = num2 + num1;
+                num1 = num2;
+                num2 = num;
+                System.out.print(num+" ");
+            }
+        }
+    }
+}
