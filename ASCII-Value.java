b625dff4e61f7d85b7dca7ea08519d52e6470b1e
Akshay R <131320419+Akshay-Coded@users.noreply.github.com>
2023-08-21 03:10:54 +0530

    Create ASCII-Value.java

---
+++ ASCII-Value.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        char ch = 'a'; //defining a character
+        int n = ch; //character conversion to integer
+        System.out.println("The ASCII value of the given character: " + n); //printing ASCII value
+    }
+}
